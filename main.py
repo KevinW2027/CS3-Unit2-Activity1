ac55eaa007a140f50258d2a968b5168e7949cc61
KevinW2027 <kevin.wang2027@gbwl.org>
2025-10-27 16:31:52 +0000

    commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("hello world")
-
-
-if __name__ == "__main__":
-    main()
+import pandas as pd
+import numpy as np
+df = pd.read_csv('movies_metadata_v2.csv', encoding='iso-8859-1').dropna(axis=1,how='all')
+print(df.head())
+print('How many ros and columns in the whole Dataset')
+print(df.shape)
+print(df.info()) # Summery of columns
+budget_df = df['budget' > 1000000]
+print(budget_df.shape)
+budget_lookup = pd.Series()
+print
