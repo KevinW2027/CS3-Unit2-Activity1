6471800c0137fc4bda4dabc5bc9ab418ddc02225
KevinW2027 <kevin.wang2027@gbwl.org>
2025-10-28 19:09:24 +0000

    commit

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ print(df.head())
 print('How many ros and columns in the whole Dataset')
 print(df.shape)
 print(df.info()) # Summery of columns
-budget_df = df['budget' > 1000000]
+budget_df = df[df['budget'] > 1000000]
 print(budget_df.shape)
-budget_lookup = pd.Series()
-print
+#Create a series form the budget column
+#that uses the title column for the indicies
+budget_lookup = pd.Series(budget_df['budget'].values, index=budget_df['title'])
+print(budget_lookup)
+print(budget_lookup['Heat'])
+ 
+#define the condition to be checked:, .index is the title of the movie
+condition = (budget_lookup.index >= 'A Bag of Hammers') & (budget_lookup.index <= 'Byzantium')
+budget_lookup_a_b = budget_lookup[condition]
