9f4210b33a3373c2add1309cc0739cf205dae304
KevinW2027 <kevin.wang2027@gbwl.org>
2025-10-29 13:37:25 +0000

    commit

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ print(budget_lookup['Heat'])
 #define the condition to be checked:, .index is the title of the movie
 condition = (budget_lookup.index >= 'A Bag of Hammers') & (budget_lookup.index <= 'Byzantium')
 budget_lookup_a_b = budget_lookup[condition]
+
+# Part C, numbers as indicies
+
+runtime_lookup = pd.Series(df['title'].values, index= df['runtime'])
+runtime_lookup = runtime_lookup.sort_index()
+print(runtime_lookup)
+
+# Filter Series by removing movies that are longer than 180 min and shorter than 10min
+condition2 = (runtime_lookup.index < 180) & (runtime_lookup.index > 10)
+runtime_lookup = runtime_lookup[condition2]
+print(runtime_lookup)
+
+#how many movies are exactly 40 minutes long
+print(runtime_lookup.loc[40])
+# How many movies are 40 mins
+print(runtime_lookup.loc[40].shape)
+print(runtime_lookup.iloc[100])
